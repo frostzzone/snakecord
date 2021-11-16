585f369be93490c7953694a1ba005e88abf3a04c
frostzzone <65735427+frostzzone@users.noreply.github.com>
2021-11-16 08:17:51 -0800

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ class SnakeGame {
         this.options = options;
         for(let y = 0; y < HEIGHT; y++) {
             for(let x = 0; x < WIDTH; x++) {
-                gameBoard[y * WIDTH + x] = '🔳';
+                gameBoard[y * WIDTH + x] = '⬛';
             }
         }
     }
